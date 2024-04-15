a1cdc18da36d05858bd1c273dd2894b0e7e16831
Cosmin-Valentin <cosminv.nicola@gmail.com>
2024-04-15 11:06:14 +0300

    Object Oriented PHP & MVC Udemy course

---
+++ app/models/User.php
@@ -0,0 +1,50 @@
+<?php
+    
+class User {
+    private $db;
+
+    public function __construct() {
+        $this->db = new Database;
+    }
+
+    public function register($data) {
+        $this->db->query('INSERT INTO users (name, email, password, created_at) VALUES(:name, :email, :password, :created_at)');
+        $this->db->bind(':name', $data['name']);
+        $this->db->bind(':email', $data['email']);
+        $this->db->bind(':password', $data['password']);
+        $this->db->bind(':created_at', date('Y/m/d H:i:s'));
+        
+        return $this->db->execute() ? true : false;
+    }
+
+    public function login($email, $password) {
+        $this->db->query('SELECT * FROM users WHERE email = :email');
+        $this->db->bind(':email', $email);
+
+        $row = $this->db->single();
+
+        $hashed_password = $row->password;
+        if(password_verify($password, $hashed_password)) {
+            return $row;
+        } else {
+            return false;
+        }
+    }
+
+    public function findUserByEmail($email) {
+        $this->db->query('SELECT * FROM users WHERE email = :email');
+        $this->db->bind(':email', $email);
+        $row = $this->db->single();
+
+        return $this->db->rowCount() ? true : false;
+    }
+
+    public function getUserById($id) {
+        $this->db->query('SELECT * FROM users WHERE id = :id');
+        $this->db->bind(':id', $id);
+
+        $row = $this->db->single();
+
+        return $row;
+    }
+}
